e1412fa02f4798bcc631b605b0aebd7b668171f2
Hatmirzj <hatmirzj@gmail.com>
2022-09-15 16:02:54 +0000

    9 and 8

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,9 @@
-nteger(my_list=[]):
-    if 0 == len(my_list):
-        return None
-    return sorted(my_list)[-1]
+#!/usr/bin/python3
+def max_integer(my_list):
+    if not my_list:
+        return (None)
+    large = my_list[0]
+    for i in my_list:
+        if i > large:
+            large = i
+    return (large)
